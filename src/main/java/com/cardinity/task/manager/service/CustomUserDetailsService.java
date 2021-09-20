7c5b5bc58ba59e99f1d8c0356f0aaf9e75bd41d2
sourov <atiqsourov.sust@gmail.com>
2021-09-20 07:37:40 +0600

    introduced logger.

--- src/main/java/com/cardinity/task/manager/service/CustomUserDetailsService.java
+++ src/main/java/com/cardinity/task/manager/service/CustomUserDetailsService.java
@@ -3,26 +3,33 @@ package com.cardinity.task.manager.service;
 import com.cardinity.task.manager.entity.User;
 import com.cardinity.task.manager.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
+@Slf4j
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+    final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         User user = userRepository.findByUserName(username);
-        System.out.println(user.getUserName());
-        System.out.println(user.getPassword());
-        System.out.println(user.getUserRole());
+
+        logger.debug("Username: "+user.getUserName());
+        logger.debug("Pass hash: "+user.getPassword());
+        logger.debug("Role: "+user.getUserRole());
+
         if (user.equals(null)) {
             throw  new UsernameNotFoundException("Username is not found.");
         }
