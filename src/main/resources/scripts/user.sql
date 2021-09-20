b40727690892245c413075f8aec648608ca8cc1f
Md. Atequer Rahman Sourov <sourovbd@users.noreply.github.com>
2021-09-20 11:21:27 +0600

    Update user.sql

--- src/main/resources/scripts/user.sql
+++ src/main/resources/scripts/user.sql
@@ -1,6 +1,6 @@
 
 INSERT INTO user(id,password,username,role)
-VALUES(1,'$2a$12$tZLPGLFWrBMUg.k6u5.BlOvpCTm9YT9FOnoEwVIOchLigMVc2PR2G','sourov','ADMIN');
+VALUES(1,'$2a$12$tZLPGLFWrBMUg.k6u5.BlOvpCTm9YT9FOnoEwVIOchLigMVc2PR2G','admin','ADMIN');
 
 INSERT INTO user(id,password,username,role)
 VALUES(2,'$2a$12$tZLPGLFWrBMUg.k6u5.BlOvpCTm9YT9FOnoEwVIOchLigMVc2PR2G','user','USER');
