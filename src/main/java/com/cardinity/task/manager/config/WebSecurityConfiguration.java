ebe87644137f927511431741b1e671999b49f4c2
sourov <atiqsourov.sust@gmail.com>
2021-09-21 10:56:02 +0600

    cors & csrf disabled.

--- src/main/java/com/cardinity/task/manager/config/WebSecurityConfiguration.java
+++ src/main/java/com/cardinity/task/manager/config/WebSecurityConfiguration.java
@@ -56,6 +56,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
+                .cors()
+                .and()
+                .csrf()
+                .disable()
                 .httpBasic();
     }
 }
